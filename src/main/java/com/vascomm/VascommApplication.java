85e2a162de46356f0861a0e08517e4b0f8d4e2ef
safeiridwan <ahmadsafei.ridwan@gmail.com>
2024-06-08 20:11:14 +0700

    feat : init spring project

---
+++ src/main/java/com/vascomm/VascommApplication.java
@@ -0,0 +1,13 @@
+package com.vascomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VascommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VascommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vascomm/VascommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vascomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VascommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
